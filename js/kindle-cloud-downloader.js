a7b295c6e995ae7608b790aee67a01107d0140a3
Eriq Augustine <eriq.augustine@gmail.com>
2019-07-09 00:28:01 -0700

    Will now fetch the title and set the zip name accordingly.

--- js/kindle-cloud-downloader.js
+++ js/kindle-cloud-downloader.js
@@ -2,20 +2,37 @@
 
 window.kindleCloudDownloader = window.kindleCloudDownloader || {};
 
+// TEST
+let sql = `
+   SELECT
+      B.metadata,
+      R.piece
+   FROM
+      bookinfo B
+      JOIN resources R ON R.asin = B.asin
+`;
+
 function fetchDataURLs() {
     return new Promise(function(resolve, reject) {
         let db = openDatabase('K4Wbooks', '7', 'K4Wbooks', 0);
 
         db.transaction(function(tx) {
-            tx.executeSql('SELECT * FROM resources', [], function(tx, results) {
-                let urls = [];
+            tx.executeSql(sql, [], function(tx, results) {
+                // {title: [url, ...], ...}
+                let urls = {};
 
                 for (let i = 0; i < results.rows.length; i++) {
-                    let payload = results.rows.item(i).piece.replace(/^"|"$/g, '');
+                    let metadata = results.rows.item(i).metadata;
+                    let piece = results.rows.item(i).piece.replace(/^"|"$/g, '');
 
-                    let match = payload.match(/^data:image\/(\w+);base64,(.+)$/);
+                    let match = piece.match(/^data:image\/(\w+);base64,(.+)$/);
                     if (!match) {
                         continue;
+                    }
+
+                    let title = JSON.parse(metadata).title;
+                    if (!(title in urls)) {
+                        urls[title] = [];
                     }
 
                     let type = match[1];
@@ -23,7 +40,7 @@ function fetchDataURLs() {
                         type = 'jpg';
                     }
 
-                    urls.push({
+                    urls[title].push({
                         type: type,
                         data: match[2],
                     });
@@ -36,13 +53,15 @@ function fetchDataURLs() {
     });
 }
 
-function createZip(dataUrls) {
+function createZip(title, dataUrls) {
     let zip = new JSZip();
     let padding = Math.max(3, dataUrls.length.toString(10).length);
 
     for (let i = 0; i < dataUrls.length; i++) {
         let name = `${i.toString(10).padStart(padding, '0')}.${dataUrls[i].type}`;
-        zip.file(name, dataUrls[i].data, {base64: true});
+        let path = `${title}/${name}`;
+
+        zip.file(path, dataUrls[i].data, {base64: true});
     }
 
     return zip;
@@ -50,10 +69,20 @@ function createZip(dataUrls) {
 
 function downloadBook(callback) {
     fetchDataURLs().then(function(urls) {
-        let archive = createZip(urls);
-        archive.generateAsync({type:"blob"}).then(function(blob) {
-            saveAs(blob, "hello.zip");
-        });
+        if (Object.keys(urls).length == 0) {
+            return;
+        }
+
+        if (Object.keys(urls).length > 1) {
+            console.error(`Multiple books found (${Object.keys(urls)}). Pin just one.`);
+        }
+
+        for (let title in urls) {
+            let archive = createZip(title, urls[title]);
+            archive.generateAsync({type:"blob"}).then(function(blob) {
+                saveAs(blob, `${title}.zip`);
+            });
+        }
     });
 }
 
